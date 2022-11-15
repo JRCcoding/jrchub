47f80216598685d1c4d6f93a93a6403df96d3c4f
Josh Claxton <jrccode@outlook.com>
2022-11-15 12:12:46 -0600

    cleanup

--- frontend/src/Screens/Notescreen.js
+++ frontend/src/Screens/Notescreen.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Notes from '../Components/Notes'
 import Navigation from '../Components/Navigation'
-import { Button, Container } from 'react-bootstrap'
-import { Link } from 'wouter'
+// import { Button, Container } from 'react-bootstrap'
+// import { Link } from 'wouter'
 
 const Notescreen = () => {
   return (
     <div>
       <Navigation />
-      <Container>
+      {/* <Container>
         <Link href='/'>
           <Button variant='dark'>Home</Button>
         </Link>
-      </Container>
+      </Container> */}
       <Notes />
     </div>
   )

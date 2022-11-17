409e8a7c20b0f30fc875f83dc09d86903f0c44de
Josh Claxton <jrccode@outlook.com>
2022-11-16 19:44:31 -0600

    .

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const app = express()
 connectDB()
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder up in production
-  app.use(express.static('client/build'))
+  app.use(express.static('frontend/build'))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
 }
 

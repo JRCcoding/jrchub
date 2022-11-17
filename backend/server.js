df3cd85cac116a1e94545eef95d8a7661f189020
Josh Claxton <jrccode@outlook.com>
2022-11-17 08:13:06 -0600

    server js heroku prep

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,21 @@ dotenv.config()
 
 const app = express()
 connectDB()
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
 
 app.use('/api/blogposts', blogRoutes)
 app.use('/api/notes', noteRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
